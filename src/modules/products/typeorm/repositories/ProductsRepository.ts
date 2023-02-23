372d5f265ef0aff056dc7cf6d0c7dcff28a00ff8
Pedro Ribeiro <ribeiro.tpedro@gmail.com>
2023-02-23 08:07:22 -0300

    serviço para criar produtos

--- src/modules/products/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/typeorm/repositories/ProductsRepository.ts
@@ -3,7 +3,6 @@ import Product from '../entities/Product';
 
 @EntityRepository(Product)
 export class ProductRepository extends Repository<Product> {
-
   public async findByName(name: string): Promise<Product | undefined> {
     const product = this.findOne({
       where: {
