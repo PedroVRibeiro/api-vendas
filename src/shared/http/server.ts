bcf06b2c7147fa643ec905e6d8ec87d5bfd12baa
Pedro Ribeiro <ribeiro.tpedro@gmail.com>
2024-09-24 10:33:19 -0300

    create basic server configuration

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,11 @@
-console.log('Hello!');
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(3333, () => {
+  console.log('Server running on port 3333!');
+});
