8353e77da40fe7aca63cdf8902cada9a847d1a10
Pedro Ribeiro <ribeiro.tpedro@gmail.com>
2024-09-25 16:55:48 -0300

    enable use of routes to server

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -3,11 +3,14 @@ import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import AppError from '../errors/AppError';
 import '@shared/typeorm';
+import routes from './routes';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(routes);
 
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
