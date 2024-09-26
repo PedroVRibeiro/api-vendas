47130947ea7028b7acb6ecf5d618c0937a30ec06
Pedro Ribeiro <ribeiro.tpedro@gmail.com>
2024-09-26 14:05:53 -0300

    create service for creating users

---
+++ src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,31 @@
+import { getCustomRepository } from 'typeorm';
+import AppError from '@shared/errors/AppError';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import User from '../typeorm/entitites/User';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export default class CreateUserService {
+  public async execute({ name, email, password }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const emailExists = await usersRepository.findByEmail(email);
+
+    if (emailExists) {
+      throw new AppError('Email already registered.');
+    }
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
