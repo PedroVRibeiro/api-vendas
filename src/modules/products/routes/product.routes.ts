d039aab68aae1b826ebe1e9be4556e269bc609f5
Pedro Ribeiro <ribeiro.tpedro@gmail.com>
2024-09-26 11:06:17 -0300

    validate ID on GET and DELETE routes

--- src/modules/products/routes/product.routes.ts
+++ src/modules/products/routes/product.routes.ts
@@ -1,13 +1,33 @@
 import { Router } from 'express';
 import ProductController from '../controllers/ProductController';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 const productRouter = Router();
 const productController = new ProductController();
 
 productRouter.get('/', productController.getAllProducts);
-productRouter.get('/:id', productController.findProduct);
+
+productRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productController.findProduct,
+);
+
 productRouter.post('/', productController.createProduct);
 productRouter.put('/:id', productController.updateProduct);
-productRouter.delete('/:id', productController.deleteProduct);
+
+productRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productController.deleteProduct,
+);
 
 export default productRouter;
