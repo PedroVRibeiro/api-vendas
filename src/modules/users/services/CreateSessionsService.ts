6c9260d777936be23cfd3f6737155031137d0a87
Pedro Ribeiro <ribeiro.tpedro@gmail.com>
2024-10-01 13:36:49 -0300

    add JWT to authentication

--- src/modules/users/services/CreateSessionsService.ts
+++ src/modules/users/services/CreateSessionsService.ts
@@ -3,17 +3,23 @@ import AppError from '@shared/errors/AppError';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
 import User from '../typeorm/entities/User';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 interface IRequest {
   email: string;
   password: string;
 }
 
+interface IResponse {
+  user: User;
+  token: string;
+}
+
 export default class CreateSessionsService {
   public async execute({
     email,
     password,
-  }: IRequest): Promise<User | undefined> {
+  }: IRequest): Promise<IResponse | undefined> {
     let passwordConfirmed;
 
     const usersRepository = getCustomRepository(UsersRepository);
@@ -27,6 +33,14 @@ export default class CreateSessionsService {
       throw new AppError('Incorrect email or password.', 401);
     }
 
-    return user;
+    const token = sign({}, '47ff3fe29f91e072a1c97a231f92d083', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return {
+      user,
+      token,
+    };
   }
 }
