b0294814e9592ecc3d57d66d96af02a377a82c8a
Pedro Ribeiro <ribeiro.tpedro@gmail.com>
2025-01-15 17:29:46 -0300

    adding get-logged-user and update-user services to controller

--- src/modules/users/controllers/UsersController.ts
+++ src/modules/users/controllers/UsersController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import ListUsersService from '../services/ListUsersService';
 import CreateUserService from '../services/CreateUserService';
+import GetUserService from '../services/GetUserService';
+import UpdateUserService from '../services/UpdateUserService';
 
 export default class UsersController {
   public async getAllUsers(
@@ -29,5 +31,37 @@ export default class UsersController {
     });
 
     return response.json(result);
+  }
+
+  public async getUser(
+    request: Request,
+    response: Response,
+  ): Promise<Response> {
+    const getUserService = new GetUserService();
+    const user_id = request.user.id;
+
+    const user = await getUserService.execute({ user_id });
+
+    return response.json(user);
+  }
+
+  public async updateUser(
+    request: Request,
+    response: Response,
+  ): Promise<Response> {
+    const updateUserService = new UpdateUserService();
+    const user_id = request.user.id;
+
+    const { name, email, password, old_password } = request.body;
+
+    const user = await updateUserService.execute({
+      user_id,
+      name,
+      email,
+      password,
+      old_password,
+    });
+
+    return response.json(user);
   }
 }
